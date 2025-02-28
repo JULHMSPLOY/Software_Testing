9dbde4988433ee7324306aae8a5111f02fb0595b
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-02-28 14:34:04 +0700

    Add test case for funny string 2

--- tests/test_funny_string.py
+++ tests/test_funny_string.py
@@ -4,3 +4,5 @@ import unittest
 class FunnyStringTest(unittest.TestCase):
     def test_funny_string_case_1(self):
         self.assertEqual(funnyString("acxz"), "Funny")
+
+    def test_funny_string_case_2(self):
