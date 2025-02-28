2629549e9cc00542f914b80bfd922d112a8b0270
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-02-28 23:00:28 +0700

    Change test case for funny string 1

--- tests/test_alternating_characters.py
+++ tests/test_alternating_characters.py
@@ -3,7 +3,9 @@ import unittest
 
 class AlternatingCharactersTest(unittest.TestCase):
     def test_alternating_characters_case1(self):
-        self.assertEqual(alternatingCharacters("AAAA"), 3)
+        s = "AAAA"
+        k = alternatingCharacters(s)
+        self.assertEqual(k, 3)
 
     def test_alternating_characters_case2(self):
         self.assertEqual(alternatingCharacters("BBBBB"), 4)
