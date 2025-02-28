726fd0a6f26284b723418b39ff1bd421cef6ddfb
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-02-28 16:24:08 +0700

    Test funny string with 'ab' * 50'

--- tests/test_funny_string.py
+++ tests/test_funny_string.py
@@ -69,6 +69,7 @@ class FunnyStringTest(unittest.TestCase):
         self.assertEqual(funnyString("abc@cba"), "Not Funny")
 
     def test_funny_string_case_23(self):
+        self.assertEqual(funnyString("ab" * 50), "Not Funny")
 
     if __name__ == '__main__':
         unittest.main()
