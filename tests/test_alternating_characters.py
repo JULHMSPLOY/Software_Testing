36fb2fe85214db41df4ac64a44eebf2494bdd10b
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-02-28 17:47:47 +0700

    Add test case for alternating characters 3

--- tests/test_alternating_characters.py
+++ tests/test_alternating_characters.py
@@ -7,3 +7,5 @@ class AlternatingCharactersTest(unittest.TestCase):
 
     def test_alternating_characters_case2(self):
         self.assertEqual(alternatingCharacters("BBBBB"), 4)
+
+    def test_alternating_characters_case3(self):
