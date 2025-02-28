3f36cac22e28740b52a29ede1b57857400d8489c
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-02-28 20:11:06 +0700

    Shift uppercase character by k

--- algorithm_codes/caesar_cipher.py
+++ algorithm_codes/caesar_cipher.py
@@ -6,3 +6,4 @@ def caesarCipher(s, k):
         if 'a' <= char <= 'z':
             new_char = chr(((ord(char) - ord('a') + k) % 26) + ord('a'))
         elif 'A' <= char <= 'z':
+            new_char = chr(((ord(char) - ord('A') + k) % 26) + ord('A'))
