1df2904a7a5cd9fa7de0a705bc3f01121070c82d
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-02-28 22:20:37 +0700

    Add test case for Caesar cipher 2

--- tests/test_caesar_cipher.py
+++ tests/test_caesar_cipher.py
@@ -4,3 +4,5 @@ import unittest
 class CaesarCipherTest(unittest.TestCase):
     def test_caesar_cipher_case1(self):
         self.assertEqual(caesarCipher("middle-Outz", 2), "okffng-Qwvb")
+    
+    def test_caesar_cipher_case2(self):
