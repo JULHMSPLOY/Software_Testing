c17ed2ea12c12c7f5bfa8ebf4b4c1a7ee2cb1b19
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-02-28 16:44:04 +0700

    Test funny string with 'abba '

--- tests/test_funny_string.py
+++ tests/test_funny_string.py
@@ -84,6 +84,7 @@ class FunnyStringTest(unittest.TestCase):
         self.assertEqual(funnyString(" abba"), "Not Funny")
 
     def test_funny_string_case_28(self):
+        self.assertEqual(funnyString("abba "), "Not Funny")
 
     if __name__ == '__main__':
         unittest.main()
