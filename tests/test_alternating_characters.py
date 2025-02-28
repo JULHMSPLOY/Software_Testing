9998d07ae0c5676e69d1d8346d87659e596ef0c2
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-02-28 17:37:19 +0700

    Add test case for alternating characters 1

--- tests/test_alternating_characters.py
+++ tests/test_alternating_characters.py
@@ -2,3 +2,4 @@ from algorithm_codes.alternating_characters import alternatingCharacters
 import unittest
 
 class AlternatingCharactersTest(unittest.TestCase):
+    def test_alternating_characters_case1(self):
