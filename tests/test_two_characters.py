2d4824ab6810c42e9b3c8d95a8917964622a6214
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-03-01 15:03:24 +0700

    Test alternate function with 'beabeefeab'

--- tests/test_two_characters.py
+++ tests/test_two_characters.py
@@ -3,3 +3,6 @@ import unittest
 
 class AlternateTest(unittest.TestCase):
     def test_two_characters_case1(self):
+        s = "beabeefeab"
+        result = alternate(s)
+        self.assertEqual(result)
