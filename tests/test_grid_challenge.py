96a8e21bc7f5272aba1bbc9b80172fa879f491b7
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-03-01 16:16:30 +0700

    Test grid challenge  with 'ebacd', 'fghij', 'olmkn', 'trpqs', 'xywuv'

--- tests/test_grid_challenge.py
+++ tests/test_grid_challenge.py
@@ -1,5 +1,8 @@
-from algorithm_codes.grid_challenge import girdChallenge
+from algorithm_codes.grid_challenge import gridChallenge
 import unittest
 
 class GridChallengeTest(unittest.TestCase):
     def test_grid_challenge_case1(self):
+        grid = ['ebacd', 'fghij', 'olmkn', 'trpqs', 'xywuv']
+        result = gridChallenge(grid)
+        self.assertEqual(result, "YES")
