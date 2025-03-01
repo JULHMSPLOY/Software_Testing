ee9bde29b6d302a3046611323eed317a8f81aa46
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-03-01 16:40:29 +0700

    Test grid challenge  with 'a', 'b', 'c', 'd', 'e'

--- tests/test_grid_challenge.py
+++ tests/test_grid_challenge.py
@@ -28,3 +28,6 @@ class GridChallengeTest(unittest.TestCase):
         self.assertEqual(result)
 
     def test_grid_challenge_case6(self):
+        grid = ['a', 'b', 'c', 'd', 'e']
+        result = gridChallenge(grid)
+        self.assertEqual(result)
