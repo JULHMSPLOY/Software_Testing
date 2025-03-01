da29d9d9ca2010721488547a083b70fe8e1a285b
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-03-01 16:36:40 +0700

    Test grid challenge  with 'zyx', 'wvu', 'tsr'

--- tests/test_grid_challenge.py
+++ tests/test_grid_challenge.py
@@ -18,3 +18,6 @@ class GridChallengeTest(unittest.TestCase):
         self.assertEqual(result)
 
     def test_grid_challenge_case4(self):
+        grid = ['zyx', 'wvu', 'tsr']
+        result = gridChallenge(grid)
+        self.assertEqual(result)
