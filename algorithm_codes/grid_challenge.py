18147c07c608dde586c4639f8d6584e675035db2
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-03-01 15:54:29 +0700

    Return 'NO' if not sorted

--- algorithm_codes/grid_challenge.py
+++ algorithm_codes/grid_challenge.py
@@ -4,3 +4,4 @@ def girdChallenge(grid):
     for col in range(len(grid[0])):
         for row in range(1, len(sorted_grid)):
             if sorted_grid[row][col] < sorted_grid[row - 1][col]:
+                return "NO"
