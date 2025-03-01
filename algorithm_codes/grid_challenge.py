c9b1891467082947a149847c74f63da58739bb08
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-03-01 17:33:48 +0700

    Sort each row in the grid

--- algorithm_codes/grid_challenge.py
+++ algorithm_codes/grid_challenge.py
@@ -1,5 +1,5 @@
 def gridChallenge(grid):
-    sorted_grid = [''.join(sorted(row)) for now in grid]
+    sorted_grid = [''.join(sorted(row)) for row in grid]
 
     for col in range(len(grid[0])):
         for row in range(1, len(sorted_grid)):
