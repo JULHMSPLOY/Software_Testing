a30f8bf8d96dac48460a82e8dc78d8dedbbbaab4
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-03-02 14:48:44 +0700

    Add test case for grid challenge 29

--- tests/test_grid_challenge.py
+++ tests/test_grid_challenge.py
@@ -170,5 +170,7 @@ class GridChallengeTest(unittest.TestCase):
         result = gridChallenge(grid)
         self.assertEqual(result, expected_output)
 
+    def test_grid_challenge_case29(self):
+
 if __name__ == '__main__':
     unittest.main()
