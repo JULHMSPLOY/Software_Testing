6113d611eeb73c04104561d56e971d0f2ee1384c
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-03-02 14:30:01 +0700

    Test alternate function with 'a b c d'

--- tests/test_two_characters.py
+++ tests/test_two_characters.py
@@ -86,5 +86,11 @@ class AlternateTest(unittest.TestCase):
         result = alternate(s)
         self.assertEqual(result, expected_output)
 
+    def test_two_characters_case15(self):
+        s = "a b c d"
+        expected_output = 0
+        result = alternate(s)
+        self.assertEqual(result, expected_output)
+
 if __name__ == '__main__':
     unittest.main()
