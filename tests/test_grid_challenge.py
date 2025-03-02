3dad5f10dadf47235d26bfa273ef5bad94f95c7b
Chotika Massapruek <chotikamassapruek@gmail.com>
2025-03-02 14:46:32 +0700

    Test gridChallenge with 'abc', 'defgh'

--- tests/test_grid_challenge.py
+++ tests/test_grid_challenge.py
@@ -159,6 +159,10 @@ class GridChallengeTest(unittest.TestCase):
         self.assertEqual(result, expected_output)
 
     def test_grid_challenge_case27(self):
+        grid = ['abc', 'defgh']
+        expected_output = "NO"
+        result = gridChallenge(grid)
+        self.assertEqual(result, expected_output)
 
 if __name__ == '__main__':
     unittest.main()
